608eec64d589e13cb574f770d3188d4f99848ec1
JaredShaneP <136398741+JaredShaneP@users.noreply.github.com>
2024-01-30 14:39:39 -0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -10,7 +10,9 @@ public class Circle {
     }
 
     public Circle(Circle copy) {
-        this.radius = copy.radius;
+        if (copy != null) {
+            this.radius = copy.radius;
+        }
     }
 
     @Override
@@ -19,31 +21,35 @@ public class Circle {
     }
 
     public boolean equals(Circle rhs) {
-        return radius == rhs.radius;
+        return this == rhs || (rhs != null && radius == rhs.radius);
     }
 
-    // or, preferred:
-
+    // Preferred:
     @Override
     public boolean equals(Object rhs) {
-        return rhs == this
-                || rhs instanceof Circle circle && radius == circle.radius;
+        if (rhs == this) {
+            return true;
+        }
+        if (rhs instanceof Circle) {
+            Circle circle = (Circle) rhs;
+            return radius == circle.radius;
+        }
+        return false;
     }
 
     public double getArea() {
         return Math.PI * radius * radius;
-    }}
-
-    Circle[] circles = new Circle[10];
-
-    for(
-    int i = 0;i<circles.length;i++)
-    {
-        circles[i] = new Circle(i + 1);
     }
 
-    for(
-    Circle circle:circles)
-    {
-    System.out.println(circle.getArea());
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[10];
+
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(i + 1);
+        }
+
+        for (Circle circle : circles) {
+            System.out.println(circle.getArea());
+        }
+    }
 }
