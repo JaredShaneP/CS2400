153eb0ba63849784a9c68f980dd4eb3fd34787cd
JaredShaneP <136398741+JaredShaneP@users.noreply.github.com>
2024-02-03 15:27:08 -0800

    Update Circle.java
    Outputs the Area of a circle after the user inputs the radius.

--- Circle.java
+++ Circle.java
@@ -1,55 +1,15 @@
-public class Circle {
-    private double radius;
-
-    public Circle() {
-        this(Math.PI);
-    }
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public Circle(Circle copy) {
-        if (copy != null) {
-            this.radius = copy.radius;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Circle[radius=%.2f]", radius);
-    }
-
-    public boolean equals(Circle rhs) {
-        return this == rhs || (rhs != null && radius == rhs.radius);
-    }
-
-    // Preferred:
-    @Override
-    public boolean equals(Object rhs) {
-        if (rhs == this) {
-            return true;
-        }
-        if (rhs instanceof Circle) {
-            Circle circle = (Circle) rhs;
-            return radius == circle.radius;
-        }
-        return false;
-    }
-
-    public double getArea() {
-        return Math.PI * radius * radius;
-    }
-
-    public static void main(String[] args) {
-        Circle[] circles = new Circle[10];
-
-        for (int i = 0; i < circles.length; i++) {
-            circles[i] = new Circle(i + 1);
-        }
-
-        for (Circle circle : circles) {
-            System.out.println(circle.getArea());
-        }
-    }
+import java.util.Scanner;
+import javax.script.ScriptContext;
+public class CircleArea
+{
+public static void main(String[] args)
+{
+Scanner keyboard = new Scanner(System.in);
+double radius;
+System.out.print("Enter the radius? ");
+radius = keyboard.nextDouble();
+System.out.print("Area of a circle with radius "
++ radius + " is "
++ (Math.PI * radius * radius) + ".");
+}
 }
