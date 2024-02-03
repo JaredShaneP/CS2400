3508e9a7493f6dc986c75f60823cc019dd380fa2
JaredShaneP <136398741+JaredShaneP@users.noreply.github.com>
2024-02-03 15:30:55 -0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,13 +3,13 @@ import javax.script.ScriptContext;
 public class CircleArea
 {
 public static void main(String[] args)
-{
+     {
+       
 Scanner keyboard = new Scanner(System.in);
 double radius;
 System.out.print("Enter the radius? ");
 radius = keyboard.nextDouble();
-System.out.print("Area of a circle with radius "
-+ radius + " is "
-+ (Math.PI * radius * radius) + ".");
-}
+System.out.print("Area of a circle with radius " + radius + " is " + (Math.PI * radius * radius) + ".");
+       
+    }
 }
